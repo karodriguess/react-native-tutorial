07fa0cd03a85f8dc5dc54936632df21aabac1d31
karodirguess <karinyrodrigues2004@gmail.com>
2024-02-15 17:04:19 -0300

    estudo-view

--- App.js
+++ App.js
@@ -4,8 +4,21 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text style={style.boasvindas}>My App</Text> */}
       <StatusBar style="auto" />
+
+      <View style={style.header}>
+        <View style={style.avatar}></View>
+          <Text style={style.boasvindas}>Kariny</Text>
+      </View>
+
+      <View style={style.body}>
+        <Text style={style.boasvindas}>Body</Text>
+      </View>
+
+      <View style={style.footer}>
+        <Text style={style.boasvindas}>Footer</Text>
+      </View>
     </View>
   );
 }
@@ -16,5 +29,42 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 40
   },
+  boasvindas: {
+    color: '#fff',
+    fontSize: 20,
+    width: 'auto',
+  },
+  header: {
+    flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems:'center',
+    backgroundColor:'green'
+  },
+  body: {
+    flex: 3,
+    width: '100%',
+    backgroundColor: 'blue',
+    lignItems: 'center',
+    justifyContent: 'center',
+   
+  },
+  footer: {
+    flex: 1,
+    width: '100%',
+    lignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor:'purple'
+   
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: '50%',
+    backgroundColor:'#fff',
+    marginHorizontal:10,
+  }
 });
